2742430a20b3369e5863c8f0ead2b1f32bd4d819
malcolm staso <mstaso1@gmail.com>
2020-06-05 01:57:28 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Genre
+  
+  attr_accessor :name
+  
+  @@all=[]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end  
+  
+  def self.all
+    @@all
+  end  
+  
+  def songs
+     Song.all { |song| song.genre == self }
+  end 
+  
+  def artists
+   songs.map(&:artist)
+  end
+  
+  
+end  
